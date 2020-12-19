335243a22fc99edd11f54d0e8dd6d9d7a77d889d
Monis Khan <immoniskhan@gmail.com>
2020-12-19 21:31:53 +0530

    COLORBOX: Added Copied message overlay and started using hooks

--- src/Components/Palette/ColorBox.jsx
+++ src/Components/Palette/ColorBox.jsx
@@ -1,67 +1,36 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import { useState } from 'react'
 import {CopyToClipboard} from 'react-copy-to-clipboard'
+import styles from './ColorBox.module.css'
 
-export class ColorBox extends Component {
-    render() {
-        const { name, color } = this.props
-        const ColorBox = styled.div`
-        background-color:${color};
-        width: 20%;
-        height:25%;
-        margin:0 auto;
-        display: inline-block;
-        position: relative;
-        cursor: pointer;
-        
-        &:hover button{
-            opacity:1;  
-            transition:0.5s;
-        }
-        
-        & button{
-            text-transform:uppercase;
-            width:100px;
-            height:30px;
-            font-size:1rem;
-            opacity:0;
-            outline:none;
-            border:none;
-            color:#fff;
-            background-color: rgb(255,255,255,0.4);
-            line-height:30px;
-            cursor:pointer;
-            position:absolute;
-            top:50%;
-            left:50%;
-            transform:translate(-50%,-50%)
-        }
-        & .nameColor{
-            display:flex;
-            text-transform:uppercase;
-            justify-content:space-between;
-            letter-spacing: 1px;
-        }
-        
-        & .colorName{
-            padding:5px;
-        }
-        & .seeMore{
-            color:#fff;
-            padding:5px;
-            background-color: rgb(255,255,255,0.4);
-        }
-        `
-        return (
-            <CopyToClipboard text={color}>
-                <ColorBox>
-                    <button>copy</button>  
-                    <div className="nameColor">
-                        <span className="colorName">{name}</span>
-                        <span className="seeMore">MORE</span>
-                    </div>
-                </ColorBox>
-            </CopyToClipboard>
-        )
+export const ColorBox = props => {
+    const { name, color } = props
+    const [isCopied, setIsCopied] = useState(false)
+
+    const handleCopy = () => {
+        setIsCopied(true)
+        setTimeout(()=>{
+            setIsCopied(false)
+        },1000)
     }
+        
+    return (
+        <CopyToClipboard text={color} onCopy={handleCopy}>
+            <div className={styles.colorBox} style={{background: `${color}`}}>
+            { isCopied && <div 
+                    style={{background: `${color}`}}
+                    className={ styles.showTime }
+                >
+                    <div className={ styles.copyMessage }>
+                        <h1>Copied!!!</h1>
+                        <p>{color}</p>
+                    </div>
+                </div> }
+                <button>copy</button>
+                <div className={styles.nameColor}>
+                    <span className={styles.colorName}>{name}</span>
+                    <span className={styles.seeMore}>MORE</span>
+                </div>
+            </div>
+        </CopyToClipboard>
+    )
 }
--- src/Components/Palette/Palette.jsx
+++ src/Components/Palette/Palette.jsx
@@ -1,29 +1,18 @@
-import React, { Component } from 'react'
-import {ColorBox} from './ColorBox'
-import styled from 'styled-components'
+import { ColorBox } from './ColorBox'
+import styles from './Pallete.module.css'
 
-export class Palette extends Component {
-    render() {
-        const colorBoxes = this.props.palette.colors?.map( item => 
-                    <ColorBox key={item.name} 
-                    color={item.color}
-                    name={item.name} 
-                    /> )
-        
-        const Palette = styled.div`
-            height:100vh;
+export const Palette = props => {
+    const colorBoxes = props.palette.colors?.map( item => 
+        <ColorBox key={item.name} 
+        color={item.color}
+        name={item.name} 
+        /> )
 
-            & .Palette-colors{
-                height:90%;
-            }
-        `
-        return (
-            <Palette>
-                {/* // Navbar */}
-                <div className="Palette-colors">{colorBoxes}</div>
-                {/* footer */}
-            </Palette>
-        )
-    }
+    return (
+    <div className={styles.main}>
+        {/* // Navbar */}
+        <div className={styles.paletteColors}>{ colorBoxes }</div>
+        {/* footer */}
+    </div>
+    )
 }
-
